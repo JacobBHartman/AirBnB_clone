9a4471eac0ac7532ad1a218f62321c3b7e4c265c
JacobBHartman <209@holbertonschool.com>
2018-02-14 21:28:49 +0000

    test_state now performs basic tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -18,3 +18,11 @@ class TestStateClass(unittest.TestCase):
     def tearDown(self):
         """ delete an instance of a 'State' """
         del self.state
+
+    def test_is_instance(self):
+        """ test if an instance is a of class 'State' """
+        self.assertIsInstance(self.state, State)
+
+    def test_name(self):
+        """ test if 'name' of a 'State' is a string """
+        self.assertIsInstance(self.state.name, str)
